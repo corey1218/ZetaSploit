1e254794b6348d346005311bcc4d773200d7c96d
Ivan Nikolsky <enty8080@gmail.com>
2020-12-08 11:05:08 +0300

    Update reverse_tcp.py

--- modules/macos/zeterpreter/reverse_tcp.py
+++ modules/macos/zeterpreter/reverse_tcp.py
@@ -151,7 +151,7 @@ class ZetaSploitModule:
             self.thread.join()
 
     def shell(self, controller):
-        plugins = self.loader.load_plugins('zeterpreter', 'multi', controller)
+        plugins = self.loader.load_plugins('zeterpreter', 'macos', controller)
         self.menus.main_plugins_menu(plugins, 'zeterpreter')
             
     def run(self):
