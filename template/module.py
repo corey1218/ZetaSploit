242a06d224f2f9ddf5ad2383f5cad67a959bb7a8
Ivan Nikolsky <enty8080@gmail.com>
2020-12-14 09:04:52 +0300

    Update module.py

--- template/module.py
+++ template/module.py
@@ -1,1 +1,35 @@
+from core.badges import badges
 
+class ZetaSploitModule:
+    def __init__(self):
+        self.badges = badges()
+        
+        self.details = {
+            'Name': "",
+            'Authors': [
+                ''
+            ],
+            'Description': "",
+            'Comments': [
+            ]
+        }
+        
+        self.options = {
+            'OPTION': {
+                'Description': "",
+                'Value': "",
+                'Required': True
+            }
+        }
+        
+        self.commands = {
+            'command': {
+                ''
+            }
+        }
+        
+    def command(self):
+        pass
+      
+    def run(self):
+        pass
