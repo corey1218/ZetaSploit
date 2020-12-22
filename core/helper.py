6d7b180dfb4d6d7a7f0fbe1ccdf641b8d9d006e6
Ivan Nikolsky <enty8080@gmail.com>
2020-12-22 14:29:28 +0100

    Update helper.py

--- core/helper.py
+++ core/helper.py
@@ -31,6 +31,7 @@ class helper:
     def __init__(self):
         self.version = "v1.0"
         self.base_path = os.path.expanduser("~") + '/.zsf/'
+        self.loot_path = self.base_path + '/loot'
 
     def getip(self):
         try:
