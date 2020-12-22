c2c5f1b6826374f49ac3dfc248ae62fbb948746b
Ivan Nikolsky <enty8080@gmail.com>
2020-12-22 18:50:13 +0100

    Update uninstall.sh

--- uninstall.sh
+++ uninstall.sh
@@ -29,10 +29,6 @@ S="\033[1;32m[+] \033[0m"
 E="\033[1;31m[-] \033[0m"
 P="\033[1;77m[>] \033[0m"
 
-clear
-cat banner/banner.txt
-echo
-
 while [[ $(sudo -n id -u 2>&1) != 0 ]]; do
     {
         sudo -v -p "$(echo -e -n $P)Password for $(whoami): " 
