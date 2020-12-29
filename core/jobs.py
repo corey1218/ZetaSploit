43430fa3b3fbd2e4d83e5aae1ae82b05160608e3
Ivan Nikolsky <enty8080@gmail.com>
2020-12-29 17:24:07 +0100

    Update jobs.py

--- core/jobs.py
+++ core/jobs.py
@@ -62,6 +62,11 @@ class jobs():
             self.badges.output_error("Invalid job id!")
             raise self.exceptions.GlobalException
         
+    def remove_dead(self):
+        for job_id in self.storage.get("jobs").keys():
+            if not self.check_alive(job_id):
+                self.storage.delete_element("jobs", job_id)
+        
     def check_jobs(self):
         if not self.storage.get("jobs"):
             return True
