8d10fcc7d31704d6592b32a207d56835ec6b213c
Ivan Nikolsky <enty8080@gmail.com>
2021-01-02 18:32:03 +0100

    Update module.py

--- core/menus/module.py
+++ core/menus/module.py
@@ -51,7 +51,7 @@ class module:
         while True:
             try:
                 current_module = self.storage.get_array("current_module", self.storage.get("pwd"))
-                commands, arguments = self.io.input('(zsf: ' + self.modules.get_category(current_module.details['Name']) + ': \033[1;31m' + self.modules.get_name(current_module.details) + '\033[0m)> ')
+                commands, arguments = self.io.input('(zsf: ' + self.modules.get_category(current_module.details['Name']) + ': \033[1;31m' + self.modules.get_name(current_module.details['Name']) + '\033[0m)> ')
                 if commands == []:
                     continue
                 else:
