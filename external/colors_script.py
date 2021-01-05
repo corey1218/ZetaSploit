65cd9243b6d437a1052c1d077ef4f7ce382ffdba
Ivan Nikolsky <enty8080@gmail.com>
2021-01-05 15:17:51 +0100

    Update colors_script.py

--- external/colors_script.py
+++ external/colors_script.py
@@ -67,16 +67,17 @@ class colors_script:
         return lines
 
     def _reversed_find_last_commands(self, lines):
-        buffer_commands = ""
+        buffer_commands = list()
         for line in lines:
             buffer_line = line
             for command in self.commands.keys():
                 if command in buffer_line:
                     buffer_line = buffer_line.replace(command, " ")
             if buffer_line.isspace():
-                buffer_commands += line.strip()
+                buffer_commands.append(line.strip())
             else:
                 break
+        buffer_commands.reverse()
         return buffer_commands
         
     def _reversed_remove_empty_lines(self, lines):
@@ -96,6 +97,7 @@ class colors_script:
         lines = self._read_file_lines(path)
         reversed_lines = self._reverse_read_lines(path)
         last_commands = self._reversed_find_last_commands(reversed_lines)
+        last_commands = "".join(map(str, last_commands))
         lines = self._reversed_remove_empty_lines(reversed_lines)
         lines[-1] = lines[-1].strip('\n') + last_commands
         if path.endswith(self.script_extension):
