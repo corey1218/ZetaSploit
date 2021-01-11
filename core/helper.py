66f5c578ed1c287ccea75275b314f1d23856d2ee
Ivan Nikolsky <enty8080@gmail.com>
2021-01-11 14:01:32 +0100

    Update helper.py

--- core/helper.py
+++ core/helper.py
@@ -53,10 +53,3 @@ class helper:
 
     def len_line(self, line):
         return str(len(line.encode())) + " bytes"
-
-    def sort_dictionary(self, dictionary):
-        new_dictionary = dict()
-        sorted_keys = sorted(dictionary.keys())
-        for key in sorted_keys:
-            new_dictionary[key] = dictionary[key]
-        return new_dictionary
