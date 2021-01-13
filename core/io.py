9bdc9ebef0611cfe79abb5444ef70da7152fbbd1
Ivan Nikolsky <enty8080@gmail.com>
2021-01-13 16:05:48 +0100

    Add files via upload

--- core/io.py
+++ core/io.py
@@ -37,7 +37,7 @@ class io:
     def output(self, message, end=os.linesep):
         sys.stdout.write('\033[1K\r' + message + end)
         sys.stdout.flush()
-        if not self.storage.get("current_prompt") and self.storage.get("active_input"):
+        if self.storage.get("current_prompt") and self.storage.get("active_input"):
             sys.stdout.write('\033[1K\r' + self.storage.get("current_prompt") + readline.get_line_buffer())
             sys.stdout.flush()
 
