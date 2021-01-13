b6d4af3b2f7a89e65e5f13b5378ee40f3410ce9d
Ivan Nikolsky <enty8080@gmail.com>
2021-01-13 18:23:13 +0100

    Update importer.py

--- core/importer.py
+++ core/importer.py
@@ -129,5 +129,5 @@ class importer:
 
     def import_all(self):
         self.import_commands()
-        self.db.add_modules(self.config.path_config['base_paths']['db_path'] + self.config.db_config['base_dbs']['modules_database'])
-        self.db.add_plugins(self.config.path_config['base_paths']['db_path'] + self.config.db_config['base_dbs']['plugins_database'])
+        self.db.add_modules('zsf_modules_db', self.config.path_config['base_paths']['db_path'] + self.config.db_config['base_dbs']['modules_database'])
+        self.db.add_plugins('zsf_plugins_db', self.config.path_config['base_paths']['db_path'] + self.config.db_config['base_dbs']['plugins_database'])
